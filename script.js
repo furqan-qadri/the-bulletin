11cfb3b7bd72d2637e5a87f55a5a96cc821e2a5d
Furqan Qadri <32431583+furqan-qadri@users.noreply.github.com>
2023-09-07 00:41:05 +0800

    broken bottombar

---
+++ script.js
@@ -0,0 +1,26 @@
+const date= new Date();
+const dateOptions = {
+    weekday: "long",
+    year: "numeric",
+    month:"long",
+    day:"numeric",
+};
+
+const dateFormatted= date.toLocaleDateString("en", dateOptions);
+const dateSelector= document.querySelectorAll(".date");
+
+for (let selector of dateSelector){
+    selector.innerHTML=dateFormatted;
+}
+
+//slick js
+
+$(function (){
+    $('.categories-slider').slick({
+        ininite:true,
+        arrows:false,
+        variableWidth:true,
+        swipeToSlide: true,
+        touchThreshold:1000,
+    })
+})
